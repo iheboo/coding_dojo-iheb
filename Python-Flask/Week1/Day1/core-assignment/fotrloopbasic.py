b77a0dd21319fe5435b8f4afcc87ecf92b16de69
iheb <jabriiheb32@gmail.com>
2022-10-24 14:35:48 -0700

    inisale

---
+++ Python-Flask/Week1/Day1/core-assignment/fotrloopbasic.py
@@ -0,0 +1,34 @@
+# basic
+for i in range(151):
+    print(i)
+# multiples_of_five
+for i in range(5,10001):   
+    if i % 5 ==0:
+        print(i)
+# counting,the dojo way
+for i in range (1,101,5):
+    if i % 10 == 0:
+         print("coding")
+    elif i % 5== 0:
+        print("coding dojo")
+    else :
+        print(i)
+# whoa
+sum = 0
+for i in range(1,5001,2):
+   sum = sum + i 
+print(sum)
+#connt
+for i in range(2018,0,-4):
+    print(i)
+# fexible
+low_num = 2
+high_num = 9
+mult= 3
+for i in range(low_num,high_num +1):
+    if i % mult == 0 :
+        print(i)
+ 
+
+    
+
