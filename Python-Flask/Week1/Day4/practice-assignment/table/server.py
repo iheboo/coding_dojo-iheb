684a36f8e1d8d653de0e7a378e6924731aa3338f
iheb <jabriiheb32@gmail.com>
2022-10-29 14:32:57 -0700

    unsatisation

--- Python-Flask/Week1/Day4/practice-assignment/table/server.py
+++ Python-Flask/Week1/Day4/practice-assignment/table/server.py
@@ -1,14 +1,14 @@
 from flask import Flask,render_template
 app=Flask(__name__)
-@app.route('/table')
+@app.route('/')
 def index():
-    users = [ 
+    users_ef = [ 
         {'first_name' : 'Michael', 'last_name' : 'Choi'},
         {'first_name' : 'John', 'last_name' : 'Supsupin'},
         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
         {'first_name' : 'KB', 'last_name' : 'Tonel'}
     ]
-    return render_template("index.html",users=users)
+    return render_template("index.html",users=users_ef)
 
 
 if __name__=="__main__": 
