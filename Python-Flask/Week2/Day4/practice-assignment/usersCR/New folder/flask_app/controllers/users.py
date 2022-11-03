3b8c45330a4723fbf6a1272be1f5bc246b30771f
iheb <jabriiheb32@gmail.com>
2022-11-03 12:13:18 -0700

    change

--- Python-Flask/Week2/Day4/practice-assignment/usersCR/New folder/flask_app/controllers/users.py
+++
@@ -1,7 +0,0 @@
-from flask_app import app
-from flask import render_template,redirect,request,session,flash
-from flask_app.models.user import User
-
-@app.route('/')
-def index():
-    return  render_template("index.html")
