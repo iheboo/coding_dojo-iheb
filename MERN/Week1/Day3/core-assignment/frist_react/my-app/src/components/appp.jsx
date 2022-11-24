4720995e01c010e3d5376b828161ace7b216b675
iheb <jabriiheb32@gmail.com>
2022-11-24 03:01:31 -0800

    commit

---
+++ MERN/Week1/Day3/core-assignment/frist_react/my-app/src/components/appp.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Appp extends Component {
+  render() {
+    return (
+      <div>
+        <h1>hi dojo</h1>
+        <li>
+          
+        </li>
+
+      </div>
+    )
+  }
+}
