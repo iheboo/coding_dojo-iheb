df466d2d0f3e815f80e3584ad9b481ae47e3ed1d
iheb <jabriiheb32@gmail.com>
2022-12-04 09:35:08 -0800

    rout

--- MERN/Week2/Day3/practice-assignment/rout/routing/src/App.js
+++ MERN/Week2/Day3/practice-assignment/rout/routing/src/App.js
@@ -3,14 +3,15 @@ import Home from './components/Home';
 import {Routes,Route} from 'react-router-dom'
 
 
+
 function App() {
   return (
     <div className="App">
       <Routes>
-      <Route path='/home' element={<h1>welcome</h1>}/>
-        <Route path='/' element={<Home/>}/>
+        <Route path='/home' element={<h1>welcome</h1>}/>
+        <Route path='/:homeId' element={<Home/>}/>
+        <Route path='/:homeId/:color1/:color2' element={<Home/>}/> 
       </Routes>
-
     </div>
   );
 }
